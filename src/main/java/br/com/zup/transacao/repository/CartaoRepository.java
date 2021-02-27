0958a4e9739bf36debf470e77bdbe91b639fc16f
taissasiilva2@gmail.com <taissasantos>
2021-02-27 12:05:48 -0300

    ajuste de pastas

--- src/main/java/br/com/zup/transacao/repository/CartaoRepository.java
+++ src/main/java/br/com/zup/transacao/repository/CartaoRepository.java
@@ -6,14 +6,6 @@ import br.com.zup.transacao.model.Cartao;
 
 public interface CartaoRepository extends JpaRepository<Cartao, Long> {
 	
-	//TypedQuery<Cartao> query = manager.createQuery(" select c from Cartao c where numero = :numero", Cartao.class);
-    //query.setParameter("numero", id);
-    //List<Cartao> resultList = query.getResultList();
 	
-	/*
-	 * @Query("SELECT c from Cartao c where c.numero = :numero AND c.id = :id")
-	 * public List<Cartao> findByNumeroCartaoAndId(@Param("numero") String
-	 * numero, @Param("id")String id);
-	 */
 
 }
